cdd80463a771618215439c9d554a15ce2cfccc7b
Sumitverma0 <vermasumit5494@gmail.com>
2024-10-21 20:28:38 +0530

    Introduction to JavaScript & Installing Node.js
    intro of the java script
    use how to use
    installation of node js and npm
    THAT'S ALL..

---
+++ 54_video/script.js
@@ -0,0 +1,8 @@
+alert("kya hal hai")
+
+console.log("code is running...")
+console.log("this is the video no. 54")
+// this console . log will show in the inspect console it is help in dubug the code 
+
+var a = prompt("enter a no. ")
+console.log("your no. is " + a)
