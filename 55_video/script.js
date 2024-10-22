b2f4fa43de07725c85f2a56899dbba53853d3317
Sumitverma0 <vermasumit5494@gmail.com>
2024-10-22 16:37:56 +0530

    JavaScript Variables, Data Types & Objects
    we learn about the basic like what is var types and var vs let
    then what is constant
    then intro of the object
    THAT'S ALL..

---
+++ 55_video/script.js
@@ -0,0 +1,31 @@
+console.log("hey this is the second class of the java script")
+
+
+var a =5;
+var b=6;
+var c ="sumit"
+
+console.log(a+b)
+console.log(typeof a,typeof b , typeof c)
+
+// const constant will not change once it create
+// let is use for the block value but var is a global variable 
+
+let d=6;
+let e=7;
+console.log(d+e)
+
+// premetive data types 
+//  null, number, string, symbol, undefined, boolean, big int 
+// IN this null is a object some example of object is 
+
+let o ={
+    "name":"sumit",
+    "roll no": 110,
+    "live_in_noida":true
+}
+
+console.log(o);
+// to add item in o
+o.salary ="100cr";
+console.log(o);
