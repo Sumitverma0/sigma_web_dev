343a564ae164acddf5d63d2df90b1bbf56c44955
Sumitverma0 <vermasumit5494@gmail.com>
2024-10-23 20:13:38 +0530

    operator or conditional statement
    totally basic of the operator or conditional nothing is different
    but one is new
    
    === and == are different
    finally their are three term =,==and === which is mention in the code
    THAT'S ALL..

---
+++ 56_video/index.js
@@ -0,0 +1,24 @@
+console.log("hello this is a conditional tutorial")
+
+let a = 45;
+if (a > 18) {
+    console.log("you can apply for driving lincence")
+}
+else {
+    console.log("you can not apply for the driving lincence")
+}
+// basic operator arithemetic operator like + - * / **  % inc dec
+// assignment operator +=,-=,=,etc
+// comparision operator ==,!=,=== equal value and type,!== not equal value not equal type,<,>,<=,>=,? ternary oprator
+// for === check use repl read evaluate print evaluate "3"==3 is true but actully it is not true therefore "3"===3 then it gives false 
+
+// logical oprator &&,||,!
+
+
+// then conditional statements like if , if else, if (else if ) else, 
+
+// ternary operator
+s = 9
+w = 5
+d = s > w ? (s - w) : (w - s);
+console.log(d)
