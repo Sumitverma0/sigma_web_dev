2b5cf48da46fc7f26030640eee483a9c4883b23e
Sumitverma0 <vermasumit5494@gmail.com>
2024-10-28 20:17:16 +0530

    Exercise no 10
    to make the random name  for the business
    THAT'S ALL..

---
+++ 62_video/index.js
@@ -0,0 +1,21 @@
+/*create a business name generator by combining list of adjectives and shop name and another words 
+
+use function and string 
+
+
+adjectives:
+crazy 
+amazing 
+fire  
+
+shop name:
+engine
+foods
+garments 
+
+ another words:
+ bros 
+ limmited
+ hub
+*/
+
