4d99b7c1acb4020a14f6bf0334f665a16ce1bf38
Sumitverma0 <vermasumit5494@gmail.com>
2024-10-29 22:11:09 +0530

    Exercise 10
    the solution of exercise no. 10
    THAT'S ALL

--- 62_video/index.js
+++ 62_video/index.js
@@ -18,4 +18,39 @@ garments
  limmited
  hub
 */
+let rand = Math.random()
+let first,second,third
+if(rand<0.34){
+    first="crazy"
+}
+else if(rand>0.34 && rand<0.64){
+    first ="amazing"
+}
+else{
+    first="fire"
+}
 
+//second
+let rand2=Math.random()
+if(rand2<0.34){
+    second="engine "
+}
+else if(rand2>0.34 && rand2<0.64){
+    second ="food"
+}
+else{
+    second="garments"
+}
+//third
+let rand3= Math.random()
+if(rand3<0.34){
+    third="bros"
+}
+else if(rand3>0.34 && rand3<0.64){
+    third ="limited"
+}
+else{
+    third="hub"
+}
+
+console.log(first,second,third)
