6eb128a76ef20bc493b9eb366afce9dd7e4b87a3
Sumitverma0 <vermasumit5494@gmail.com>
2024-11-01 13:36:05 +0530

    EXERCISE 11
    find the factorial using for loop and reduce in which i complete with for loop
      THAT'S ALL..

---
+++ 65_video/index.js
@@ -0,0 +1,14 @@
+let n = prompt("enter a no. to find the factorial")
+let fact = 1;
+
+console.log("the for loop")
+// for loop factorial 
+for (let index = 1; index <=n; index++) {
+    fact = fact * index;
+}
+console.log(fact);
+
+
+
+console.log("the reduce ")
+// i can't understand how to find factorial by using an array i think using for loop enter the value in an array then we have to find the fact by using the another for loop by access the element of an array that' it 
