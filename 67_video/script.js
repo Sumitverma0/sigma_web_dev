21c762fd12f525a3e7a85f2ab90159050d7e5cf1
Sumitverma0 <vermasumit5494@gmail.com>
2024-11-03 20:52:48 +0530

    JavaScript DOM - Children, Parent & Sibling Nodes
    just access the childnodes , parentnodes, and some another nodes to target we can use css in it also
    THAT'S ALL.

---
+++ 67_video/script.js
@@ -0,0 +1,34 @@
+console.log("hello world")
+// In console
+// hello world
+// document.body
+// <body>​…​</body>​
+// document.body.childNodes
+// NodeList(5) [text, div.container, text, script, text]
+// document.body.childNodes[0]
+// #text
+// document.body.childNodes[1]
+// <div class=​"container">​…​</div>​
+// document.body.childNodes[1].childNodes
+// NodeList(11) [text, div.box, text, div.box, text, div.box, text, div.box, text, div.box, text]
+
+// let counte = document.body.childNodes[1]
+// undefined
+// counte
+// <!-- text here -->
+// let counte = document.body.childNodes[2]
+// undefined
+// counte
+// #text
+// let counte = document.body.childNodes[3]
+// undefined
+// counte
+// <div class=​"container">​…​</div>​
+// counte.firstElementChild
+// <div class=​"box">​box1​</div>​
+
+// for parent we use .parentelement
+
+// some tables method 
+
+// these is not need to learn we use in project automatic learn 
