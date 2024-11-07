c90aeecf9a0b467743b09be1b3f4ddeaeadc0a25
Sumitverma0 <vermasumit5494@gmail.com>
2024-11-07 19:55:51 +0530

    DOM inserting and removing elements
    using dom property which in mention in js file with the help of comment how to manipulate the elements
    THAT'S ALL..

---
+++ 71_video/script.js
@@ -0,0 +1,33 @@
+// here we first use --> document.querySelector(".box")
+// output:- <div class=​"box">​hey! i am a box ​</div>​
+// input --> document.querySelector(".box").innerHTML
+// output :- 'hey! i am a box '
+// use innertext it will provide the text only suppose we use in container then it will show the text 
+
+// outer html is give all html in it and it also 
+//tagName is give the tag name of it . it is only for element name futher we use nodeName 
+//node name is for all node
+ // textcontent it give the text content 
+ // hidden give the true or false if we change it then it will appear as we choosen    
+ 
+ 
+ // innerhtml = "kuch bhi likho voo change appper hoga"
+ // hasAttribute("style") gives true or false
+// if hasAttribute is true then with the help of getAttribute we will get that attribute 
+
+// now we change the attribute with the help of setAttribute  for example-->.("style", display.inline)
+
+// removeAttribute will remove the attribute 
+
+// AMAZING 
+// document.designMode = "on"  will help to edit the webpage mannully just amazing use it 
+
+
+// data-created by = "sumit verma" yai kisi bhi attribute mai add kar saktai hooo bina kisi error kai to help it. to see it just use document.queryselector(".box").dataset then it will give all data-kuchbhi 
+
+// .append is use to add at the last of the data 
+// yai dynamically use hota hai agar kahi sai data aata hai too hmm esai use kartai hai 
+
+// .classlist
+// .classlist add and remove
+// toggle it will change it if on then change into off and if off then change into on
