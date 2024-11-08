4c977915e1e993f758fa29f44fab5bdddb40ba10
Sumitverma0 <vermasumit5494@gmail.com>
2024-11-08 22:21:47 +0530

    Exercise 13
    just make a card of video details shown
    THAT'S ALL..

---
+++ 73_video/script.js
@@ -0,0 +1,11 @@
+function createcard(tittle,cname,views,monthold,duration,thumbnail){
+    // finish this function
+}
+
+// steps 
+// first insert the image for thumbnail and add some css like height width padding margin 
+// complete with the help of function just show the details of the video 
+// for views if four digit or five digit then divide by 1000 and apply the K AT last
+// if the no. of views is greater then 5 digit or between 6 digit divide by 10000 and apply M soo on B 
+// for date how month old just use calender 
+// i think the steps is ok for this project 
